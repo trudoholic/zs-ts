6ad479d3f3b55d211f3f7cb40ce9c1758d223618
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-06 22:08:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default App
